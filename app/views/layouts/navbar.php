0710a62188e033513211cb2b79848805b7f08b6d
Praveen Siriwardena <smpmsiri23@gmail.com>
2025-05-08 03:16:22 +0530

    navigation bar - pt1

---
+++ app/views/layouts/navbar.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+
+<body>
+    <header class="bg-gray-800 text-white pr-10 pl-10 pt-4 pb-4">
+        <nav class="flex justify-between items-center mx-auto w-[90%]">
+            <div>
+                <h1>GameNova</h1>
+            </div>
+            <div class="absolute md:static md:min-h-fit md:w-auto bg-gray-800 w-full h-16 top-[9%] left-0 min-h-[60vh] flex item-center"></div>
+            <ul class="flex md:flex-row flex-col items-center md:gap-[4vw] gap-10">
+                <li><a href="">Home</a></li>
+                <li><a href="">Products</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Wishlist</a></li>
+                <li><a href="">Cart</a></li>
+            </ul>
+            </div>
+            <div>
+                <button>
+                    <img src="/cb008920/public/assets/images/main/avatar-white.png" alt="profile" class="w-7 h-7 ">
+                </button>
+            </div>
+        </nav>
+    </header>
+</body>
+
+</html>
